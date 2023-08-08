549c1f28b46da5d79e3bf3670d6810eaa624d8b8
h4rpy-wings <hassaniyaqoti@gmail.com>
2023-08-08 16:55:57 +0330

    wip

--- lib/Basket.sol
+++ lib/Basket.sol
@@ -17,16 +17,27 @@ contract Basket {
   address public _baseToken;
   address payable public _admin;
   
-  uint256 private _lockedFunds;
 
   uint256 public _totalLiquidity;
   uint256 public _availableLiquidity;
+  uint256 public _lockedFunds;
 
+
+  IterableMapping.Map private _withdrawRequests;
 
   IterableMapping.Map private _funds;
   IterableMapping.Map private _queuedFunds;
-
+  mapping (address => uint256) _releasedFunds;
   mapping (address => uint256) _profits;
+
+  // totalLiquidity = availbaleLiquidity + lockedFunds; 
+  // totalLiquidity = queuedFunds + _releasedFunds + _profits + _lockedFunds
+  // withdrawableFunds = queuedFunds + _releasedFunds
+  // availableLiquidity >= _releasedFunds + _profits + _queuedFunds
+  //
+  // --QueuedFunds-->|---->>>----|--ReleasedFunds-->
+  //                 |LockedFunds|--Profits-->
+  //                 |----<<<----|
 
   constructor(address _owner,address _admin, address _baseToken, uint256 _ownerFund) {
     _owner = _owner;
@@ -60,6 +71,50 @@ contract Basket {
   // the owner or admin can call this function to specify the amount of loss
   // todo add hash of Positions to this function
   function burn(uint256 _amount) public _onlyOwner() returns (bool) {}
+
+  // ─── Withdraw ────────────────────────────────────────────────────────
+
+  // returns the withdrawable profit for the account.
+  function withdrawableProfit(address _account) public view returns (uint256) {
+    return _profits[_account];
+  }
+
+  // return the total withdrawable funds for this account in this basket
+  function withdrawableFund(address _account) public view returns (uint256) {
+    return _releasedFunds[_account].add(IterableMapping.get(_queuedFunds, _account));
+  }
+
+  // return the queued funds for this account in this basket
+  function queuedFund(address _account) public view returns (uint256) {
+    return IterableMapping.get(_queuedFunds, _account);
+  }
+
+  // return the released funds for this account in this basket
+  function releasedFund(address _account) public view returns (uint256) {
+    return _releasedFunds[_account];
+  }
+
+
+  function withdrawProfit(uint256 _amount) public returns (uint256) {}
+
+  // withdrawProfit allows the admin to do a withdraw for the _account.
+  // the founds will trasfer from the Contract to the account.
+  function withdrawProfit(uint256 _amount, address _account) public returns (bool) {
+  }
+
+  function withdrawFund(uint256 _amount) public returns (bool) {}
+  function withdrawFund(uint256 _amount, address _account) public returns (bool) {}
+
+
+
+  function _withdrawProfit(uint256 _amount,address _account) internal returns (bool) {}
+  function _withdrawFund(uint256 _amount,address _account) internal returns (bool) {}
+  
+  // reinvest from the Profit gained
+  function reinvestFromProfit(uint256 _amount) public returns (bool) {}
+
+  // reivest on the behalf of the owner by the admin 
+  function reinvestFromProfit(uint256 _amount, address _from) _onlyAdmin() public returns (bool) {}
 
   
   // ─── Modifiers ───────────────────────────────────────────────────────
