5471162aa3d28997f5bea90e91fff4b008205884
h4rpy-wings <hassaniyaqoti@gmail.com>
2023-08-09 23:35:59 +0330

    wip

--- lib/Basket.sol
+++ lib/Basket.sol
@@ -15,21 +15,23 @@ contract Basket {
   status private _status;
   uint256 public _ownerFund;
   uint64 private _iteration;
+  uint64 public _maximumFund;
   address public _baseToken;
   address public _admin;
   
 
   uint256 public _totalLiquidity;
   uint256 public _availableLiquidity;
-  uint256 public _lockedFunds;
-
+  uint256 public _totalLockedFunds;
+  uint256 private _totalWithdrawRequests;
+  uint256 private _totalQueuedFunds;
 
   IterableMapping.Map private _withdrawRequests;
 
-  IterableMapping.Map private _funds;
+  IterableMapping.Map private _lockedFunds;
   IterableMapping.Map private _queuedFunds;
-  mapping (address => uint256) _releasedFunds;
-  mapping (address => uint256) _profits;
+  mapping (address => uint256) public _releasedFunds;
+  mapping (address => uint256) public _profits;
 
   bytes4 private _transferFromSelector;
   bytes4 private _transferSelector;
@@ -58,7 +60,7 @@ contract Basket {
 
   // returns total liquidity of the contract
   function totalLiquidity() public returns (uint256) {
-    return availableLiquidity().add(_lockedFunds);
+    return availableLiquidity().add(_totalLockedFunds);
   }
 
   // returns the amount of available Liquidity of the contract
@@ -82,11 +84,11 @@ contract Basket {
 
   // the owner or admin can call this function to specify the amount of profit
   // todo add hash of Positions to this function
-  function profitShare(uint256 _amount) public _onlyOwner() returns (bool) {}
+  function profitShare(uint256 _amount,bytes memory _signature) public _onlyOwner() returns (bool) {}
 
   // the owner or admin can call this function to specify the amount of loss
   // todo add hash of Positions to this function
-  function burn(uint256 _amount) public _onlyOwner() returns (bool) {}
+  function burn(uint256 _amount,bytes memory _signature) public _onlyOwner() returns (bool) {}
 
   // ─── Withdraw ────────────────────────────────────────────────────────
 
@@ -105,40 +107,69 @@ contract Basket {
     return IterableMapping.get(_queuedFunds, _account);
   }
 
-  // return the released funds for this account in this basket
-  function releasedFund(address _account) public view returns (uint256) {
-    return _releasedFunds[_account];
+  function withdrawProfit(uint256 _amount) public returns (bool) {
+    return _withdrawProfit(_amount, msg.sender);
   }
-
-
-  function withdrawProfit(uint256 _amount) public returns (uint256) {}
 
   // withdrawProfit allows the admin to do a withdraw for the _account.
   // the founds will trasfer from the Contract to the account.
   function withdrawProfit(uint256 _amount, address _account) public returns (bool) {
+    return _withdrawProfit(_amount, _account);
   }
 
-  function withdrawFund(uint256 _amount) public returns (bool) {}
-  function withdrawFund(uint256 _amount, address _account) public returns (bool) {}
+  function withdrawFund(uint256 _amount) public returns (bool) {
+    return _withdrawFund(_amount, msg.sender);
+  }
 
-
+  function withdrawFund(uint256 _amount, address _account) public returns (bool) {
+    return _withdrawFund(_amount, _account);
+  }
 
   function _withdrawProfit(uint256 _amount,address _account) internal returns (bool) {
     _profits[_account] = _profits[_account].sub(_amount);
+    return __transfer(_amount, _account);
+  }
 
+  function _withdrawFund(uint256 _amount,address _account) internal returns (bool) {
+    _queuedFunds.set(_account, _releasedFunds[_account].add(_queuedFunds.get(_account)).sub(_amount));
+    _releasedFunds[_account] = 0;
+    return __transfer(_amount, _account);
+  }
+
+  function __transfer(uint256 _amount,address _account) internal returns (bool) {
     (bool success,) =_baseToken.call(abi.encodeWithSelector(_transferSelector, _account, _amount));
     require(success,"Transfering from contract failed");
-    
+    _availableLiquidity = _availableLiquidity.sub(_amount);
+    return true;
   }
-  function _withdrawFund(uint256 _amount,address _account) internal returns (bool) {}
+
+  // ─── Investing ───────────────────────────────────────────────────────
+
+  function invest(uint256 _amount, bytes memory _signature) public
+    _isActive() 
+    _isAcceptable(_amount) 
+    _mustBeAllowedToInvest(_amount,msg.sender,_signature) 
+    _mustBeTransferred(_amount,msg.sender,address(this)) 
+    returns (bool) {
+    _queuedFunds.set(msg.sender, _queuedFunds.get(msg.sender).add(_amount));
+    _totalQueuedFunds = _totalQueuedFunds.add(_amount);
+  }
   
   // reinvest from the Profit gained
-  function reinvestFromProfit(uint256 _amount) public returns (bool) {}
+  function reinvestFromProfit(uint256 _amount) public returns (bool) {
+    return _reinvestFromProfit(_amount, msg.sender);
+  }
 
   // reivest on the behalf of the owner by the admin 
-  function reinvestFromProfit(uint256 _amount, address _from) _onlyAdmin() public returns (bool) {}
+  function reinvestFromProfit(uint256 _amount, address _from) _onlyAdmin() public returns (bool) {
+    return _reinvestFromProfit(_amount, _from);
+  }
 
-  
+  function _reinvestFromProfit(uint256 _amount, address _from) _isAcceptable(_amount) internal returns (bool) {
+    _profits[_from] = _profits[_from].sub(_amount);
+    _queuedFunds.set(_from, _queuedFunds.get(_from).add(_amount));
+    return true;
+  }
   // ─── Modifiers ───────────────────────────────────────────────────────
 
   modifier _isActive() {
@@ -156,5 +187,21 @@ contract Basket {
     _;
   }
 
+  modifier _mustBeTransferred(uint256 _amount, address _from,address _to) {
+    (bool _success, ) = _baseToken.call(abi.encodeWithSelector(_transferFromSelector,_from, _to, _amount));
+    require(_success,"Transfering from _contract failed");
+    _availableLiquidity = _availableLiquidity.add(_amount);
+    _;
+  }
+
+  modifier _mustBeAllowedToInvest(uint256 _amount, address _from, bytes memory _signature) {
+    // todo invest signature {amount(uint256),from(address),expiration(blockHeight)}
+    _;
+  }
+
+  modifier _isAcceptable(uint256 _amount) {
+    require(_totalQueuedFunds.add(_totalLockedFunds).add(_amount).sub(_totalWithdrawRequests) <= _maximumFund,"the Basket is full");
+    _;
+  }
 
 }
