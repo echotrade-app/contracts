126695af8d8f1ee189e40791a0797e259dac0e9d
jusufliam <morteza@irisdev.net>
2023-09-04 18:18:36 +0330

    feat: wait for deployment

--- test/MultiSigWallet_test.ts
+++ test/MultiSigWallet_test.ts
@@ -28,93 +28,99 @@ describe("MultiSigWallet", async ()=>{
 
     describe("submitTransaction", () => {
         it("should submitTransaction", async function () {
-        
+            await contract.waitForDeployment()
+
             // Submit a transaction
-          const resp = await contract.submitTransaction(Inv1.getAddress(), Inv2.getAddress(), 1000, '0x');
-    
-          expect(resp).to.be.not.undefined;
-          expect(resp).to.be.not.null;
-          expect(resp).to.be.not.NaN;
-          expect(resp).to.equal(5);
+            const resp = await contract.submitTransaction(Inv1.getAddress(), Inv2.getAddress(), 1000, '0x');
+
+            expect(resp).to.be.not.undefined;
+            expect(resp).to.be.not.null;
+            expect(resp).to.be.not.NaN;
+            expect(resp).to.equal(5);
         });
       });
 
     describe("confirmTransaction", () => {
         it("should confirmTransaction", async function () {
-        
+            await contract.waitForDeployment()
+
             // Submit a transaction
-          const resp = await contract.confirmTransaction(10000012300);
-    
-          expect(resp).to.be.not.undefined;
-          expect(resp).to.be.not.null;
-          expect(resp).to.be.not.NaN;
-          expect(resp).to.equal(5);
+            const resp = await contract.confirmTransaction(10000012300);
+
+            expect(resp).to.be.not.undefined;
+            expect(resp).to.be.not.null;
+            expect(resp).to.be.not.NaN;
+            expect(resp).to.equal(5);
         });
     });
       
     describe("executeTransaction", () => {
         it("should executeTransaction", async function () {
-        
+            await contract.waitForDeployment()
+
             // Submit a transaction
-          const resp = await contract.executeTransaction(10000012300);
-    
-          expect(resp).to.be.not.undefined;
-          expect(resp).to.be.not.null;
-          expect(resp).to.be.not.NaN;
-          expect(resp).to.equal(5);
+            const resp = await contract.executeTransaction(10000012300);
+
+            expect(resp).to.be.not.undefined;
+            expect(resp).to.be.not.null;
+            expect(resp).to.be.not.NaN;
+            expect(resp).to.equal(5);
         });
       });
 
       describe("revokeConfirmation", () => {
         it("should revokeConfirmation", async function () {
-        
+            await contract.waitForDeployment()
+
             // Submit a transaction
-          const resp = await contract.revokeConfirmation(10000012300);
-    
-          expect(resp).to.be.not.undefined;
-          expect(resp).to.be.not.null;
-          expect(resp).to.be.not.NaN;
-          expect(resp).to.equal(5);
+            const resp = await contract.revokeConfirmation(10000012300);
+
+            expect(resp).to.be.not.undefined;
+            expect(resp).to.be.not.null;
+            expect(resp).to.be.not.NaN;
+            expect(resp).to.equal(5);
         });
       });
       
       describe("getOwners", () => {
         it("should getOwners", async function () {
-        
+            await contract.waitForDeployment()
+
             // Submit a transaction
-          const resp = await contract.getOwners();
-    
-          expect(resp).to.be.not.undefined;
-          expect(resp).to.be.not.null;
-          expect(resp).to.be.not.NaN;
-          expect(resp).to.equal(5);
+            const resp = await contract.getOwners();
+
+            expect(resp).to.be.not.undefined;
+            expect(resp).to.be.not.null;
+            expect(resp).to.be.not.NaN;
+            expect(resp).to.equal(5);
         });
       });  
       
       describe("getTransactionCount", () => {
         it("should getTransactionCount", async function () {
-        
+            await contract.waitForDeployment()
             // Submit a transaction
-          const resp = await contract.getTransactionCount();
-    
-          expect(resp).to.be.not.undefined;
-          expect(resp).to.be.not.null;
-          expect(resp).to.be.not.NaN;
-          expect(resp).to.equal(5);
+            const resp = await contract.getTransactionCount();
+
+            expect(resp).to.be.not.undefined;
+            expect(resp).to.be.not.null;
+            expect(resp).to.be.not.NaN;
+            expect(resp).to.equal(5);
         });
       });
 
 
       describe("getTransaction", () => {
         it("should getTransaction", async function () {
-        
+            await contract.waitForDeployment()
+
             // Submit a transaction
-          const resp = await contract.getTransaction(10000012300);
-    
-          expect(resp).to.be.not.undefined;
-          expect(resp).to.be.not.null;
-          expect(resp).to.be.not.NaN;
-          expect(resp).to.equal(5);
+            const resp = await contract.getTransaction(10000012300);
+
+            expect(resp).to.be.not.undefined;
+            expect(resp).to.be.not.null;
+            expect(resp).to.be.not.NaN;
+            expect(resp).to.equal(5);
         });
       });
 
